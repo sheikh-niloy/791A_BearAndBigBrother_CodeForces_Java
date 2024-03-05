16c419bd23e6d53927ce4c4109f98112ba8c8e0b
sheikh-niloy <sheikh.niloy.info@gmail.com>
2024-03-06 00:08:16 +0600

    commit

---
+++ Start.java
@@ -0,0 +1,59 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Start {
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        TaskA solver = new TaskA();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class TaskA {
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int result = 0;
+            while (a <= b) {
+                a *= 3;
+                b *= 2;
+                result++;
+            }
+            out.println(result);
+        }
+
+    }
+
+    static class InputReader {
+        private static BufferedReader in;
+        private static StringTokenizer tok;
+
+        public InputReader(InputStream in) {
+            this.in = new BufferedReader(new InputStreamReader(in));
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public String next() {
+            try {
+                while (tok == null || !tok.hasMoreTokens()) {
+                    tok = new StringTokenizer(in.readLine());
+                }
+            } catch (IOException ex) {
+                System.err.println("An IOException was caught :" + ex.getMessage());
+            }
+            return tok.nextToken();
+        }
+
+    }
+}
